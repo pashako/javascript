593a9fbadc4313fa45590b72e9fc2054ff4ccc4a
pashako <zombak01kp@gmail.com>
2019-11-10 14:00:06 +0200

    day1

---
+++ classwork/Day1/index.js
@@ -0,0 +1,92 @@
+"use strict";
+
+// console.log("Hello, loh");
+
+// let number = 64;
+// let string = "loh";
+// let Boolean = true; // false
+
+// let a = 2;
+// let b = 3;
+
+// let result = a + b;
+
+// console.log(result);
+
+// alert("Скиньте плз деньги на карточку **** **** **** **** очень надо!");
+
+
+// var c = confirm();
+// console.log(c);
+
+// const z = prompt("enter your card number");
+// const z1 = prompt("enter your password pls");
+
+// const a = 9;
+// let b = "9";
+
+// // console.log(a === Number(b));
+
+// // let a = Number("bjb");
+// // console.log(Number.isNaN(a));
+
+// console.log(0.1 + 0.3 === 0.4);
+
+// let a = 0.1 + 0.2;
+
+// console.log(a.toFixed(1))
+
+// console.log((Math.random()*10).toFixed(0))
+
+// const a = 1,
+//      b = 5,
+//      c = 8;
+
+// console.log(Math.max(a, b, c));
+
+// console.log(Math.ceil(0.5));
+//console.log(Math.floor(0.5));
+//console.log(Math.round(0.5));
+//console.log(Math.pow(0.5, 11111));
+
+// const age = 25;
+
+// console.log("hello I am " + age + " years old");
+
+
+
+
+// const name = "Rodion";
+
+// console.log(name.length);
+// console.log(name.indexOf("o"));
+// console.log(name.toLowerCase());
+// console.log(name.toUpperCase());
+// console.log(name.includes("o"));
+
+// const firstName = "Rodion";
+// const surName = "Romanov";
+// const age = 25;
+
+// console.log(`Hello my name is ${firstName} ${surName} and I am ${age} years old`);
+
+// console.log(String(true) === "true")
+
+// console.log(+"100text"); // NaN
+// console.log(+undefined); // NaN
+// console.log(+null); // 0
+// console.log(+true); // 1
+// console.log(+false); // 0
+// console.log( "1" == true); // true
+// console.log( "1" === true); // false
+
+const day = 17;
+const month = 10;
+const year = 2048;
+
+const date = day + "/" + month + "/" + year;
+const message = `доброе утро, сегодня ${date}, за бортом отличная погода!`
+
+console.log(date);
+console.log(message);
+
