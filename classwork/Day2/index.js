d148e67867954c3cb2bb96a7b7c0fdb6c63836f5
pashako <zombak01kp@gmail.com>
2019-11-17 12:50:30 +0200

    day2

---
+++ classwork/Day2/index.js
@@ -0,0 +1,112 @@
+"use strict";
+
+// const a = 20;
+
+// let result = a > 10 && a < 30;
+
+// console.log(result);
+
+// const b = 20;
+
+// let result = b < 10 || b > 10;
+
+// console.log(result);
+
+
+// const c = 20;
+
+// let result = !c < 10;
+
+// console.log(result);
+
+// const a = 11;
+// if(a === 10){
+//     alert ("Dai money")
+
+// }else if(a < 10){
+//     confirm("Dai mnogo money")
+// }else {
+//     prompt("Day ochen mnogo money");
+// }
+
+// const num = 15;
+
+// num < 10 ? console.log("xz") : console.log("xz2");
+
+// let num = prompt("enter your age");
+
+// if (+num > 5){
+//     alert("You are very old")
+// }else{
+//     alert("you are young")
+// }
+
+// const num = 20;
+
+// let result = num === 10
+// ? console.log("10")
+// :num===20
+// ?console.log("20")
+// :num===30
+// ?console.log("30")
+// :console.log("Ypu are lohg")
+ 
+// const total = 100;
+// const ordered = 50;
+
+// if (total > ordered){
+//     alert("на складе недостаточно товаров")
+// }else if (total === ordered){
+//     alert("Вы забираете весь товар со склада")
+// }else {
+//     alert("заказ оформлен за вами выехали ")
+// }
+
+// let a = prompt("введите произвольное целое число!")
+
+// if(!a){
+//     alert("Ну и ладно лох")
+// }else if (a * 10 % 10 != 0){
+//     alert("Я просил ЦЕЛОЕ число дебил")
+// }else{
+//     alert("Спасибо")
+// }
+
+// let a = prompt("Введите число *****")
+// switch(a){
+//     case 1:
+//         alert("Каталог хостелов");
+//         break;
+//     case 2:
+//         alert("Каталог бюджетных отелей");
+//         break;
+//     case 3:
+//         alert("Каталог отелей ***");
+//         break;
+//     case 4:
+//         alert("Каталог отелей ****");
+//         break;
+//     case 5:
+//         alert("Каталог лучших отелей ");
+//         break;
+//     case 0:
+//         alert("дверь слева дибил");
+//         break;
+//     default:
+
+//         break;
+    
+    
+// }
+
+// const adminLogin = 'admin';
+// const adminPassword = 'therdntuvlyhrk'
+
+// let a = prompt("login, please:");
+
+// if (a === adminLogin){
+//     let b = prompt("password, please:");
+// }else if (!a){
+
+// }else
+
